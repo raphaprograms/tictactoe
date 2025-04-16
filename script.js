c56f96f9b734a865779b3fd73ad27787ebcc7dc1
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-04-15 18:42:52 -0700

    Create gameboard
    This way the board is dynamic

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-const test = document.querySelector('h1');
-test.textContent = ' CHANGED IT!';
+
+let gameboard = function() {
+
+    let rows = 3;
+    let columns = 3;
+    const board = [];
+
+    for(i = 0; i < rows; i++) {
+        row = [];
+        for(j = 0; j < columns; j++) {
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
+}
+ console.log(gameboard());
