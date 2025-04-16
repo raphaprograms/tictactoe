f45303a18713e2c36f1a2e5c348fb75c3b89e3da
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-04-15 19:48:51 -0700

    Add placeToken method to gameBoard/createPlayer
    createPlayer factory functon is going to control player data and methods

--- script.js
+++ script.js
@@ -1,17 +1,62 @@
+// need a game board
+// need players to play
+// gameboard needs to be able to be filled in with tokens
 
-let gameboard = function() {
+function gameBoard() {
 
     let rows = 3;
     let columns = 3;
     const board = [];
 
-    for(i = 0; i < rows; i++) {
-        row = [];
-        for(j = 0; j < columns; j++) {
+    for(let i = 0; i < rows; i++) {
+        let row = [];
+
+        for(let j = 0; j < columns; j++) {
             row.push(' ');
         }
+
         board.push(row);
     }
-    return board;
+
+    function printBoard() {
+        for(let row of board){
+            console.log(row.join(' | '));
+        }
+    }
+
+    function placeToken(row, col, token) {
+        if(board[row][col] === ' '){
+            board[row][col] = token;
+        } else {
+            console.log('Cell is already taken!')
+        }
+    }
+
+    return {
+        board, 
+        printBoard,
+        placeToken,
+    };
+
 }
- console.log(gameboard());
+
+const game = gameBoard();
+game.printBoard();
+game.placeToken(1, 1, 'O');
+game.printBoard();
+
+
+ function createPlayer(name, token) {
+    return {
+        name,
+        token,
+
+        makeMove(row, col, gameBoard) {
+            gameBoard.placeToken(row, col, token);
+        }
+    }
+ };
+
+ let  = {
+
+ }
